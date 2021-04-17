788179d9f31d6ce9d5443954e6b79e0c50d2e519
amin Sehati <aminsehati70@gmail.com>
2021-04-17 16:48:28 +0430

    Respansive project

--- js/App.js
+++ js/App.js
@@ -1,7 +1,6 @@
 const btnBars = document.querySelector('.btn-bars');
 const sidbar = document.querySelector('.sidbar');
 const close = document.querySelector('.close');
-console.log(close);
 close.addEventListener('click',(e)=>{
     e.preventDefault();
     sidbar.classList.remove('active');
